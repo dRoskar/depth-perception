1573820dbe9ebdd1406fbf44572ff403c79bc781
Damjan Roskar <damjan.roskar.s@gmail.com>
2016-02-06 10:39:11 +0100

    add mouseover instruction

--- script/main.js
+++ script/main.js
@@ -1,5 +1,28 @@
-var touched = false;
-			
 $(document).ready(function(){
+	var c = $("#canvas").get(0);
+	var ctx = c.getContext("2d");
+	
+	var untouched = true;
+	
+	// mouseover canvas
+	$("#canvas").mouseover(function(){
+		if(untouched){
+			ctx.clearRect(0, 0, c.width, c.height);
+			untouched = false;
+		}
+	});
+	
+	if(untouched){
+		// add instruction rect to canvas
+		//rectangle base
+		ctx.fillStyle = "#000000";
+		ctx.fillRect(387, 195, 250, 40);
+		
+		//text
+		ctx.textAlign = "center";
+		ctx.fillStyle = "#EFDFAF";
+		ctx.font = "24px Times New Roman";
+		ctx.fillText("your mouse goes here", 512, 220);
+	}
 	
 });
