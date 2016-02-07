5edcae34893e6637c599ae3f7f1cef8319b01a7d
Damjan Roskar <damjan.roskar.s@gmail.com>
2016-02-07 15:42:02 +0100

    organize retrieved data; wait for images to load; init draw

--- script/main.js
+++ script/main.js
@@ -1,33 +1,94 @@
+ var c = null;
+ var ctx = null;
+ var content = null;
+ var untouched = true;
+ var loadedCount = 0;
+
 $(document).ready(function(){
-	var c = $("#canvas").get(0);
-	var ctx = c.getContext("2d");
-	
-	var untouched = true;
+	c = $("#canvas").get(0);
+	ctx = c.getContext("2d");
 	
 	// get canvas contents
 	$.get("backend/getContent.php?p=", function(data){
 		if(data == "false"){
-			console.log("error fetching data");
+			// error
+		}
+		else{
+			loadedCount = 0;
+			
+			// save retrieved content
+			data = JSON.parse(data);
+			
+			content = {
+				title: data[1],
+				author: (data[2] == "" || data[2] == null) ? "Anonymous" : data[2],
+				images: {},
+				limit: data[13] == "f" ? false : true,
+				front_image: data[14],
+				page: data[15]
+			};
+			
+			for(var i = 1; i < 11; i++){
+				content.images["layer" + i] = {
+						url: data[i + 2] == null ? "images/empty.gif" : data[i + 2],
+						imageObj: new Image(),
+						x: settings.canvasWidth / 2,
+						y: settings.canvasHeight / 2,
+						factor: settings["layer" + i + "Factor"]
+				};
+			}
+			
+			// suppy source to image objects and wait for all images to load
+			for(var key in content.images){
+				var url = content.images[key].url;
+				
+				// notify onload
+				content.images[key].imageObj.onload = function(){
+					loadedCount++;
+					
+					if(loadedCount == 10){
+						imagesLoaded();
+					}
+				}
+				
+				// set image src
+				content.images[key].imageObj.src = url;
+			}
 		}
 	});
+});
+
+function imagesLoaded(){
+	// begin redraw loop
+	setInterval(redraw, 10);
 	
-	// mouse enter canvas
+	// start monitoring mouse over canvas
+	$("#canvas").mousemove(glide);
+	
+	// capture first touch
 	$("#canvas").mouseover(function(){
-		if(untouched){
-			ctx.clearRect(0, 0, c.width, c.height);
-			untouched = false;
-		}
+		untouched = false;
 	});
+}
+
+function redraw(){
+	// clear canvas
+	ctx.clearRect(0, 0, c.width, c.height);
 	
-	// mouse move over canvas
-	$("#canvas").mouseover(function(){
-		// get position
-		
-		// update values
-	});
+	// draw images in correct order
+	for(var i = 1; i < 11; i++){
+		var image = content.images["layer" + i];
+		ctx.drawImage(image.imageObj, image.x - (image.imageObj.width/2), image.y - (image.imageObj.height/2));
+	}
 	
+	// show instruction
 	if(untouched){
 		// add instruction rect to canvas
+		
+		//wheat border
+		ctx.fillStyle = "#EFDFAF";
+		ctx.fillRect(386, 194, 252, 42);
+		
 		//rectangle base
 		ctx.fillStyle = "#000000";
 		ctx.fillRect(387, 195, 250, 40);
@@ -38,5 +99,8 @@ $(document).ready(function(){
 		ctx.font = "24px Times New Roman";
 		ctx.fillText("your mouse goes here", 512, 220);
 	}
-	
-});
+};
+
+function glide(){
+	// update image coordinates
+};
---
+++ script/settings.js
@@ -0,0 +1,15 @@
+var settings = {
+	canvasWidth: 1024,
+	canvasHeight: 576,
+	
+	layer1Factor: 0.025,
+	layer2Factor: 0.05,
+	layer3Factor: 0.075,
+	layer4Factor: 0.1,
+	layer5Factor: 0.125,
+	layer6Factor: 0.15,
+	layer7Factor: 0.175,
+	layer8Factor: 0.2,
+	layer9Factor: 0.225,
+	layer10Factor: 0.25
+}
